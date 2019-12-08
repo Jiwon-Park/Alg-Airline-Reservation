cd9af9120b85e4ac84b5306572875a879d1d5faa
Seniorious <wldnjs1323@naver.com>
2019-12-08 20:06:16 +0900

    우선순위 큐 완성

--- Alg-Airline-Reservation/timetable.cpp
+++ Alg-Airline-Reservation/timetable.cpp
@@ -1,5 +1,5 @@
 #include "timetable.h"
-
+#define min(a,b) a<b?b:a
 TableElement::TableElement() {};
 
 TableElement::TableElement(int s, int e, Time d) 
@@ -28,6 +28,17 @@ void Time::setTime(int h, int m)
 	minute = m;
 }
 
+bool Time::operator<(const Time right)
+{
+	if (hour < right.hour || (hour == right.hour && minute < right.minute))return true;
+	return false;
+}
+
+bool Time::operator>(const Time right)
+{
+	if (hour > right.hour || (hour == right.hour && minute > right.minute))return true;
+	return false;
+}
 
 TimeTable::TimeTable() {};
 
@@ -76,4 +87,85 @@ TableElement TimeTable::getElement(int day, int index)
 	return table[day][index];
 }
 
+TableElement* TimeTable::shortestPath(int start, int end, int day, int &transfer_times)
+{
+	//TODO
+	return nullptr;
+}
+
 TimeTable::~TimeTable() {};
+
+void priority_queue::pop()
+{
+	delete[] &(e[1]);
+	e[1] = queueelem(e[count]);
+	e[count].initialized = false;
+	count--;
+	int index = 1, index2, index3;
+	while (true)
+	{
+		index2 = e[index * 2] < e[index * 2 + 1] ? index * 2 : index * 2 + 1;
+		index3 = index2 ^ 1;
+		if (e[index] > e[index2])
+		{
+			swap(index, index2);
+			index = index2;
+		}
+		else if (e[index] > e[index3])
+		{
+			swap(index, index3);
+			index = index3;
+		}
+		else break;
+	}
+}
+
+queueelem priority_queue::top()
+{
+	return e[1];
+}
+
+void priority_queue::swap(int idx1, int idx2)
+{
+	queueelem temp(e[idx1]);
+	e[idx1] = e[idx2];
+	e[idx2] = temp;
+}
+
+void priority_queue::push(int index, int day, Time time)
+{
+	count++;
+	e[count].setelem(index, day, time);
+	int index2 = count;
+	while (index2 / 2)
+	{
+		if (e[index2] < e[index2 / 2])
+		{
+			swap(index2, index2 / 2);
+			index2 /= 2;
+		}
+		else break;
+	}
+}
+
+#undef min(a,b)
+
+void queueelem::setelem(int index, int day, Time time)
+{
+	initialized = true;
+	this->index = index;
+	this->day = day;
+	this->time.setTime(time.hour, time.minute);
+}
+
+bool queueelem::operator<(const queueelem right)
+{
+	if (day < right.day || (day == right.day && time < right.time))return true;
+	return false;
+}
+
+bool queueelem::operator>(const queueelem right)
+{
+	if (day > right.day || (day == right.day && time > right.time))return true;
+	return false;
+}
--- Alg-Airline-Reservation/timetable.h
+++ Alg-Airline-Reservation/timetable.h
@@ -10,6 +10,8 @@ public:
 	Time();
 	Time(int h, int m);
 	void setTime(int h, int m);
+	bool operator<(const Time right);
+	bool operator>(const Time right);
 };
 
 class TableElement
@@ -33,7 +35,37 @@ public:
 	void setTimeTable(Map map);
 	void showTimeTable(int day);
 	TableElement getElement(int day, int index);
+	TableElement* shortestPath(int start, int end, int day, int &transfer_times);
 private:
 	TableElement table[31][100];
 };
+
+class queueelem
+{
+public:
+	queueelem() {};
+	queueelem(int i, int d) :index(i), day(d) { initialized = true; };
+	~queueelem() {};
+	void setelem(int index, int day, Time time);
+	int index, day;
+	Time time;
+	bool initialized = false;
+	bool operator<(const queueelem right);
+	bool operator>(const queueelem right);
+};
+
+
+class priority_queue
+{
+	queueelem e[1026];
+	int count = 0;
+	void swap(int idx1, int idx2);
+public:
+	priority_queue() {};
+	~priority_queue() {};
+	void push(int index, int day, Time time);
+	void pop();
+	queueelem top();
+};
+
 #endif
